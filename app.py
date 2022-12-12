e038df9b0ce3b96729073042a075bbe069a713ae
root <root@vclvm176-51.vcl.ncsu.edu>
2022-12-12 17:41:30 -0500

    added gunicorn and implemented as a service

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ def hello_world():
     return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0',port=5000,debug=True)
